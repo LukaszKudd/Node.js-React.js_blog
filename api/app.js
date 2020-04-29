e188235b334abffd0ff68260fb1fad56c1a07878
LukaszKudd <lukasz.kuderewski@gmail.com>
2020-04-29 14:25:54 +0200

    Added login page

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var indexNews = require("./routes/news");
+var indexAdmin = require("./routes/admin");
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/news", indexNews);
+app.use("/admin", indexAdmin);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- api/routes/index.js
+++ api/routes/index.js
@@ -5,8 +5,14 @@ router.get("/", (req, res, next) => {
   res.render("index", { title: "Express" });
 });
 
-router.get("/admin", (req, res, next) => {
-  res.render("admin", { title: "Admin" });
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Sign in" });
 });
+
+// router.post("/admin", (req, res, next) => {
+//   console.log(req.body);
+
+//   res.redirect("/admin");
+// });
 
 module.exports = router;
