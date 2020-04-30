275be7c72d90a242a890f7e0fe124625374e196c
LukaszKudd <lukasz.kuderewski@gmail.com>
2020-04-30 11:47:12 +0200

    Global req.path

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
-  console.log(req.path);
+  res.locals.path = req.path;
 
   next();
 });
