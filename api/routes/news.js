e3e745b071813ed7aeaff0f29b41d2d18be469a1
LukaszKudd <lukasz.kuderewski@gmail.com>
2020-05-06 15:07:06 +0200

    Connected frontend and backend

--- api/routes/news.js
+++ api/routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.json({ text: "Hello" });
+  res.json({ text: "Hello API" });
 });
 
 module.exports = router;
--- client/src/Api.js
+++ client/src/Api.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
 class Api extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      text: "",
+    };
+  }
+
   componentDidMount() {
-    fetch("http://localhost:9000/news").then((res) => console.log(res.json()));
+    fetch("http://localhost:9000/news")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          text: result.text,
+        });
+      });
   }
 
   render() {
-    return <div>Hello</div>;
+    return <div>{this.state.text}</div>;
   }
 }
 
