cf040f8cb48965490a79346011d7a486db934a4a
LukaszKudd <lukasz.kuderewski@gmail.com>
2020-05-11 22:36:00 +0200

    Added reset css file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../CSS/reset.css";
 import "../CSS/App.css";
+import "../CSS/Header.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import "../CSS/Header.css";
 
 function App() {
   return (
