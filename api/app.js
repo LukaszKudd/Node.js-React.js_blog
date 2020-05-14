58ffdc1c39c3ba2565eb8d78dd6505f54b54f566
LukaszKudd <lukasz.kuderewski@gmail.com>
2020-05-15 00:09:58 +0200

    Added cookie parser

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 var createError = require("http-errors");
+var cookieSession = require("cookie-session");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -21,6 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [],
+
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
 app.use(function (req, res, next) {
   res.locals.path = req.path;
